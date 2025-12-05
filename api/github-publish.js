000ff3a6899db9750d8069d534d40ada201878d2
Thomas Stephens <tmsteph1290@gmail.com>
2025-12-04 20:50:56 -0800

    Fix CORS headers in github-publish.js

--- api/github-publish.js
+++ api/github-publish.js
@@ -1,6 +1,6 @@
 const setCorsHeaders = (res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 };
 
