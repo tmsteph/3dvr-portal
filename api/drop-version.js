c87c1713d7e57a06bc89537514c946688cf8b2bd
Thomas Stephens <tmsteph1290@gmail.com>
2026-09-24 13:46:23 -0700

    Remove Drop version serverless function

--- api/drop-version.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
-  res.status(200).json({
-    sha: process.env.VERCEL_GIT_COMMIT_SHA || process.env.VERCEL_DEPLOYMENT_ID || 'local'
-  });
-}
